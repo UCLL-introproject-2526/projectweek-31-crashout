3b0e0601ce17ca3490a6b82690bc8dd05bbe69dd
Jaro Deckers <jaro.deckers@student.ucll.be>
2025-12-15 14:43:58 +0100

    pygame file2

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
 import pygame
+
 pygame.init()
+screen_size = (800,600)
+screen = pygame.display.set_mode(screen_size)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+
+pygame.quit()
